3c5da4671266f3cb29f1cb641d5d5ca97d08be57
sagar <sagar_sys@live.com>
2020-03-03 23:57:50 +0400

    > Register dashboard component + DASH_FACADE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DASH_FACADE, FlownDashboardFacade, SalesDashboardFacade} from './models/dashboard.facade';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: DASH_FACADE,
+      useClass: SalesDashboardFacade,
+      multi: true
+    },
+    {
+      provide: DASH_FACADE,
+      useClass: FlownDashboardFacade,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
